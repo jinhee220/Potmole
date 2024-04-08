5a5a118f091b442a502856593ad0ca4585a98e78
Ryan Miller <ryanmillerbusiness159@gmail.com>
2024-04-08 10:41:09 -0400

    Updated data starting date in potholes table

--- java/database/data.sql
+++ java/database/data.sql
@@ -4,9 +4,14 @@ INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULi
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 -- TEST DATA --
---INSERT INTO 
-	--potholes (user_id, latitude, longitude, current_status, reported_date, inspected_date, repaired_date)
---VALUES
---	(1, 39.0000, -75.3124, 'repaired', '4-2-2024', '4-3-2024', '4-5-2014');
+INSERT INTO 
+	potholes (user_id, latitude, longitude, current_status, reported_date, inspected_date, repaired_date)
+VALUES
+	(1, 39.0000, -75.3124, 'repaired', '4-2-2024', '4-3-2024', '4-5-2014');
+	
+INSERT INTO 
+	potholes (user_id, latitude, longitude, current_status, reported_date, inspected_date, repaired_date)
+VALUES
+	(1, 45.0000, -85.3124, 'inspected', '4-2-2024', '4-3-2024', '');
 
 COMMIT TRANSACTION;
