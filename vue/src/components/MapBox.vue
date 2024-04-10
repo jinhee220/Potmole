3f568bb2c45658ea14d9284c9088d9fabc1d871a
Jay Eros <ugstrings@gmail.com>
2024-04-10 17:20:04 -0400

    Added functionality to grab street address based on map click and request user location on page load to assign map center

--- vue/src/components/MapBox.vue
+++ vue/src/components/MapBox.vue
@@ -12,8 +12,11 @@ import mapboxgl from "mapbox-gl";
 // Include Mapbox Geocoder plugin from node-module after running npm install @mapbox/mapbox-gl-geocoder in terminal
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
+
 // Import Mapbox CSS
 import "mapbox-gl/src/css/mapbox-gl.css";
+// Import Mapbox-Geocoder CSS
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css"
 
 // Import PotholeService
 import PotholeService from "@/services/PotholeService.js"
@@ -25,6 +28,7 @@ export default {
 			editCoordinates: {
 				longitude: '',
 				latitude: '',
+				streetAddress: '',
 			},
 			// Set null map variable to allow the map to updated
 			map: null,
@@ -53,56 +57,73 @@ export default {
 		this.map = new mapboxgl.Map({
 			container: this.$refs.map,
 			style: "mapbox://styles/mapbox/streets-v12",
-			center: [-79.4512, 43.6568],
-			zoom: 13,
+			center: [-98.00000, 39.00000],
+			zoom: 3,
 		});
 
-
-
+		// Check if geolocation is available for the user's browser
+		if (navigator.geolocation){
+			// Get user's current position
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					// Extract long and lat from coordinates
+					const longitude = position.coords.longitude;
+					const latitude = position.coords.latitude;
+					// Set map center and zoom to reflect user location
+					this.map.setCenter([longitude, latitude]);
+					this.map.setZoom(10);
+				}
+			)
+		}
+		
 		// Add the geocoder control to the map
 		const geocoder = new MapboxGeocoder({
 			accessToken: mapboxgl.accessToken,
 			mapboxgl: mapboxgl,
 		});
-		
+
 		// Add Geolocation for user location to mapboxgl
 		this.map.addControl(
-        new mapboxgl.GeolocateControl({
-            positionOptions: {
-                enableHighAccuracy: true
-            },
-            // When active the map will receive updates to the device's location as it changes.
-            trackUserLocation: false,
-            // Draw an arrow next to the location dot to indicate which direction the device is heading.
-            showUserHeading: true
-        }));
+			new mapboxgl.GeolocateControl({
+				positionOptions: {
+					enableHighAccuracy: true
+				},
+				// When active the map will receive updates to the device's location as it changes.
+				trackUserLocation: false,
+				// Draw an arrow next to the location dot to indicate which direction the device is heading.
+				showUserHeading: true
+			}));
 
 
 		// This adds plugin to map
 		this.map.addControl(geocoder);
 
 		// Only allows the following code to run if component is being imported into PotholeList
-		if (this.parentComponent==='PotholeList') {
-		this.getPotholeList();
+		if (this.parentComponent === 'PotholeList') {
+			this.getPotholeList();
 		}
 		// Only allows the following code to run if component is being imported into ReportPotholeView
-		if (this.parentComponent==='ReportPotholeView'){
+		if (this.parentComponent === 'ReportPotholeView') {
 
-		// This lets us click on map and get coordinates and call methods
-		this.map.on("click", (event) => {
-			const coords = event.lngLat;
+			// This lets us click on map and get coordinates and call methods
+			this.map.on("click", (event) => {
+				const coords = event.lngLat;
+				
 
-			// Call method that adds marker to the map
-			this.addMarker(coords);
+				// Call method that adds marker to the map
+				this.addMarker(coords);
 
-			// Update editCoordinates data with clicked coordinates
-			this.editCoordinates.longitude = coords.lng.toFixed(5);
-			this.editCoordinates.latitude = coords.lat.toFixed(5);
+				// Update longitude and latitude properties of editCoordinates with clicked coordinates
+				this.editCoordinates.longitude = coords.lng.toFixed(5);
+				this.editCoordinates.latitude = coords.lat.toFixed(5);
+				// Update street address property of editCoordinates by feeding clicked coordinates 
+				this.reverseGeocode(coords);
 
-			// Send coordinates-selected event with updated coordinates for parent class to listen for
-			this.$emit("coordinates-selected", this.editCoordinates);
-		});
-	}
+				// Send coordinates-selected event with updated coordinates for parent class to listen for
+				this.$emit("coordinates-selected", this.editCoordinates);
+
+			});
+		}
 	},
 
 	// Method to create an element for marker, instantiate marker, and add marker to map
@@ -110,47 +131,79 @@ export default {
 		addMarker(coords) {
 			// Check if marker is null. If it is, create element in map document and initialize marker with map.Marker
 			// If marker is not null, skip if statement and only set marker coordinates and add to map
-			if (this.marker==null){
+			if (this.marker == null) {
 				// Create a new element in instance of map
 				const markerElement = document.createElement("div");
 				// Assign class to element to be affected by style sheet
 				markerElement.className = "marker";
 				// Initialize marker with built in API mapboxgl.Marker method
-				this.marker = new mapboxgl.Marker(markerElement, {offset: [-15, 1]})
+				this.marker = new mapboxgl.Marker(markerElement, { offset: [-15, 1] })
 			}
 			// Assign coordinates to marker and add it to the map
-			
-				this.marker
+
+			this.marker
 				.setLngLat(coords)
 				.addTo(this.map);
-				
-				
 		},
 		addAllMarkers(potholeList) {
 			// Loop through potholeList and add a marker to the map for each pothole
-			for (const pothole of potholeList){
+			for (const pothole of potholeList) {
 				// Create a new element in instance of map
 				const markerElement = document.createElement("div");
 				// Assign class to element to be affected by style sheet
 				markerElement.className = "marker";
 				// Initialize marker with built in API mapboxgl.Marker method
-				const marker = new mapboxgl.Marker(markerElement, {offset: [-15, 1]})
-			
-			// Assign coordinates to marker and add it to the map
+				const marker = new mapboxgl.Marker(markerElement, { offset: [-15, 1] })
+
+
+
+				// Assign coordinates to marker and add it to the map
 				marker
-				.setLngLat([pothole.longitude, pothole.latitude])
-				.addTo(this.map);
-				
-            }	
+					.setLngLat([pothole.longitude, pothole.latitude])
+					//Add popup on marker click
+					.setPopup(
+						new mapboxgl.Popup({ offset: [100, 100] }) // add popups
+							.setHTML(
+								`<h3>${pothole.currentStatus}</h3>
+		<p>${pothole.reportedDate}</p>
+		<p>${pothole.inspectedDate}</p>
+		<p>${pothole.repairedDate}</p>`
+							)
+					)
+					.addTo(this.map);
+			}
+
+
 		},
-        getPotholeList() {
-            PotholeService.getPotholeList()
-                .then((response) => {
-                    this.potholeList = response.data;
-                    
-                    this.addAllMarkers(this.potholeList);
-                })
-        },
+		getPotholeList() {
+			PotholeService.getPotholeList()
+				.then((response) => {
+					this.potholeList = response.data;
+
+					this.addAllMarkers(this.potholeList);
+				})
+		},
+		// Call the MapBox Geocoder API and retrieve a street address based on given coordinates
+		reverseGeocode(coords) {
+			// The API URL we will retrieve data from, it takes in coordinates and retrieves the place information from those coordinates
+			const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${coords.lng},${coords.lat}.json`;
+			// Our MapBox access token, it identifies who we are and is required to communicate with the API
+			const accessToken = "pk.eyJ1IjoidGVzdHltY3Rlc3RmYWNlIiwiYSI6ImNsdW1wcjE5NjB3aGUyaW1scGVtb2R4YjIifQ._vhnmLy-i4syL82KbkdyDA";
+
+			// Fetch is a built in javascript function that is used to make HTTP Requests. It takes in a full url
+			// Our fetch requires the base api url and our access token to prove our right to access the information
+			fetch(`${apiUrl}?access_token=${accessToken}`)
+			// The fetch method sends a requests for data from the URL and returns it as a response that we convert into json for easier manipulation
+				.then((response) => response.json())
+				.then((data) => {
+					// The returned data is an object containing nested objects that we branch through to reach our 'place_name', a property that contains a full street address
+					const address = data.features[0].place_name;
+					this.editCoordinates.streetAddress = address;
+				})
+				.catch((error) => {
+					console.error("Error fetching address:", error);
+				});
+		},
 
 	},
 
@@ -161,7 +214,6 @@ export default {
 .map {
 	width: 100%;
 	height: 400px;
-	border: solid black;
 
 }
 
@@ -173,11 +225,11 @@ export default {
 /* Set css style for marker */
 .marker {
 	background-image: url('../assets/potholePointer.png');
-	background-size:contain;
+	background-size: contain;
 	background-repeat: no-repeat;
 	background-color: transparent;
 	width: 50px;
-	height:50px;
+	height: 50px;
 	cursor: pointer;
 	position: absolute;
 
