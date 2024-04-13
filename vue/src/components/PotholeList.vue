fdb7e6a64a2140a169ea9916d77f6e9ac8479f8c
Andrew Eaton <andrew.eaton02@gmail.com>
2024-04-13 16:53:20 -0400

    added filter by status on non-employee page

--- vue/src/components/PotholeList.vue
+++ vue/src/components/PotholeList.vue
@@ -1,11 +1,32 @@
 <template>
+    
     <div>
         <!-- Call MapBox component and set parentComponent prop -->
         <MapBox parent-component="PotholeList" />
     </div>
-    <div class="pothole-container">
-        <PotholeDetail v-bind:key="pothole.id" v-for="pothole in potholeList" v-bind:pothole="pothole" />
+    
+    <div class="filter">
+        <h3>For Pothole Details, Select A Status: </h3>
+            <div class="dropdown">
+                    <div class="dropdown-content">
+                        <select id="currentStatus" name="currentStatus" v-model="selectedOption">
+                            <option value="">All</option>
+                            <option value="Reported">Reported</option>
+                            <option value="Inspected">Inspected</option>
+                            <option value="Repaired">Repaired</option>
+                            <option value="Deleted">Deleted</option>
+                        </select>
+
+                        <button v-on:click="getFilteredList()">Apply Filter</button>
+
+                    </div>
+            </div>
     </div>
+    
+    <div class="pothole-container">
+        <PotholeDetail v-bind:key="pothole.id" v-for="pothole in filteredList" v-bind:pothole="pothole" />
+    </div>
+
 </template>
 
 <script>
@@ -14,25 +35,39 @@ import PotholeService from "../services/PotholeService.js";
 import MapBox from "./MapBox.vue";
 
 export default {
-    computed: {
-            //call the PotholeService.js here
-            potholeList() {
-                return this.$store.state.potholeList
-            //PotholeService.getPotholeList()
+
+    data() {
+        return {
+            selectedOption: '',
+            filteredList: this.$store.state.potholeList
+        }
     },
-},
+
+    computed: {
+        //call the PotholeService.js here
+        potholeList() {
+            return this.$store.state.potholeList
+            //PotholeService.getPotholeList()
+        },
+    },
     components: {
         PotholeDetail,
         MapBox
     },
     methods: {
 
-    },
-    created() {
-        this.$store.commit("UPDATE_POTHOLE_LIST");
-    },
-}
+        getFilteredList() {
+            this.filteredList = this.$store.state.potholeList.filter((pothole) => { return pothole.currentStatus.includes(this.selectedOption) } );
+            //return this.$store.state.potholeList.filter((pothole) => { return pothole.currentStatus.includes(this.selectedOption) } );
 
+        },
+        created() {
+            this.$store.commit("UPDATE_POTHOLE_LIST");
+            this.getFilteredList();
+        },
+
+    }
+}
 </script>
 
 <style>
@@ -40,5 +75,20 @@ export default {
     display: flex;
     justify-content: space-evenly;
     flex-wrap: wrap;
+}
+
+.filter {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+h3 {
+    padding-right: 10px;
+}
+
+#currentStatus {
+    margin-right: 10px;
 }
 </style>
